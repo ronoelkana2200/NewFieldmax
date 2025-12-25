2b37cf0d25bad454d260bd86ef33ee4579d57891
FIELDMAX <fieldmaxshop@gmail.com>
2025-12-25 14:51:12 +0300

    Update shop page and URLs

--- fieldmax/urls.py
+++ fieldmax/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from website.views import RoleBasedLoginView
 from django.contrib.auth.views import LogoutView
 
@@ -20,3 +22,8 @@ urlpatterns = [
     path('', include('website.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
+# Serve media files in development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
